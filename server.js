c3700932f19d3032a0c5a490a7fc4e7f827eec2f
Timothy Putzke <tsputzke@gmail.com>
2019-07-30 21:40:13 -0400

    Implement '/movie' path

--- server.js
+++ server.js
@@ -1,30 +1,53 @@
-// Users can search for Movies by genre, country or avg_vote
+require('dotenv').config()
+const express = require("express");
+const movies = require("./movies-data-small.json");
+const cors = require('cors')
+const helmet = require('helmet')
 
-const express = require('express');
-const movies = require('./movies-data-small.json');
+const app = express();
+app.use(helmet())
+app.use(cors())
 
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
 
-const app = express()
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
 
-// The endpoint is GET /movie
-app.get('/movie', function handleGetMovies(req, res) {
-  res.json(movies)
+  next()
 })
+
+app.get("/movie", function handleGetMovies(req, res) {
+  let response = movies;
+
+  // filter movies by genre if query param is present
+  if (req.query.genre) {
+    response = movies.filter(movie =>
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    );
+  }
+
+  // filter movies by country if query param is present
+  if (req.query.country) {
+    response = movies.filter(movie =>
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+    );
+  }
+
+  // filter movies by avg_vote if query param is present (greater than or equal to the supplied number)
+  if (req.query.avg_vote) {
+    response = movies.filter(movie => {
+      if(Number(movie.avg_vote) >= Number(req.query.avg_vote)) {
+        return movie
+      }
+    });
+  }
+
+  res.json(response);
+});
 
 app.listen(8000, () => {
-  console.log(`Server listening at http://localhost:8000`)
-})
-// The search options for genre, country, and/or average vote are provided in query string parameters.
-// When searching by genre, users are searching for whether the Movie's genre includes a specified string. The search should be case insensitive.
-// When searching by country, users are searching for whether the Movie's country includes a specified string. The search should be case insensitive.
-// When searching by average vote, users are searching for Movies with an avg_vote that is greater than or equal to the supplied number.
-// The API responds with an array of full movie entries for the search results
-// The endpoint only responds when given a valid Authorization header with a Bearer API token value.
-// The endpoint should have general security in place such as best practice headers and support for CORS.
-
-//hint:
-// When comparing two numeric strings for greater than or less than, we can "cast" the strings to numbers like so:
-
-// Number('1') === 1
-// Number('3') === 3
-// Number('6.123') === 6.123
+  console.log(`Server listening at http://localhost:8000`);
+});

c79e5900be21a63cdcc1539f431be01bcb450cdb
Timothy Putzke <tsputzke@gmail.com>
2019-08-01 11:32:19 -0400

    Prepare for Heroku deployment

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const helmet = require('helmet')
 const app = express();
 app.use(helmet())
 app.use(cors())
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN
@@ -48,6 +50,18 @@ app.get("/movie", function handleGetMovies(req, res) {
   res.json(response);
 });
 
-app.listen(8000, () => {
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:8000`);
 });
